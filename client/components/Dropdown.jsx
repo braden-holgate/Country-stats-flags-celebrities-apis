a7a542294cef99ed9448d48690696f0597dfd3f1
Nila <26017191+Nilandohnjosh@users.noreply.github.com>
2022-04-15 11:10:27 +1200

    api calls done

--- client/components/Dropdown.jsx
+++ client/components/Dropdown.jsx
@@ -1,11 +1,37 @@
 import React, { useState, useEffect } from 'react'
 import  Stats  from './Stats'
 import  Flag  from './Flag'
+import { getCountries } from '../api/country'
 
 const Dropdown = () => {
+  const [country, setCountry] = useState({
+    name: 'Afghanistan',
+    code: 'AF',
+    currency: 'AFN',
+    population: 38930000
+  })
+
+  const [c, setC] = useState(null)
+
+  // const findCountry = () => {
+  //   getCountries()
+  //     .then(str => {
+  //       console.log(str)
+  //     })
+  // }
+
+  const findCountry = async () => {
+    const countriesData = await getCountries()
+    console.log(countriesData)
+  }
+  
+  
   return (
     <>
-    <div>Dropdown</div>
+    {/* Create Dropdown menu, linking data from API */}
+    <button onClick={() => findCountry()}>Find Country</button>
+    
+  
     <Flag />
     <Stats />
     </>
@@ -14,3 +40,18 @@ const Dropdown = () => {
 }
 
 export default Dropdown
+
+// npm install -D @babel/plugin-transform-runtime@7
+
+/*
+      "plugins": [
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-object-rest-spread",
+      [
+        "@babel/plugin-transform-runtime",
+        {
+          "regenerator": true
+        }
+      ]
+    ]
+*/
